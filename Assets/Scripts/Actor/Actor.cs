0b3d3bb883429facdf53aeb07033260c0b2555f5
Ben Rokah <benk0914@gmail.com>
2019-01-26 18:27:02 +0200

    Navigation

---
+++ Assets/Scripts/Actor/Actor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Actor : MonoBehaviour
+{
+    [SerializeField]
+    NavMeshAgent NavAgent;
+
+    private void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            NavigateTo(CORE.Instance.GroundMouseHit.point);
+        }
+    }
+
+    #region Basic
+
+    void NavigateTo(Vector3 targetPosition)
+    {
+        NavAgent.SetDestination(targetPosition);
+    }
+
+    #endregion
+}
