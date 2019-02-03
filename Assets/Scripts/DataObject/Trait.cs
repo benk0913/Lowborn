eb336097befb93edab06795d8eb2b2f5383a738d
Ben Rokah <benk0913@gmail.com>
2019-02-03 18:30:50 +0200

    traits wip

---
+++ Assets/Scripts/DataObject/Trait.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Trait", menuName = "DataObjects/Trait", order = 2)]
+public class Trait : ScriptableObject
+{
+    [SerializeField]
+    public string Description;
+
+    [SerializeField]
+    public DateTime? ExpirationDate = null;
+
+    [SerializeField]
+    public List<TraitBonus> Bonuses = new List<TraitBonus>();
+
+    [SerializeField]
+    public Trait NextStageTrait;
+
+    [SerializeField]
+    public Trait CounterTrait;
+}
---
+++ Assets/Scripts/DataObject/TraitBonus.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TraitBonus", menuName = "DataObjects/TraitBonus", order = 2)]
+public class TraitBonus : ScriptableObject
+{
+    public BonusType Type;
+    public string Key;
+    public string Value;
+}
+
+[System.Serializable]
+public enum BonusType
+{
+    Relations,
+    Need,
+}
