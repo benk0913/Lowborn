b0f0699fca9e07902d5c6e8045a1f864cde78173
Jacob <JacobCohenIsrael@gmail.com>
2019-02-11 19:14:16 +0200

    POC for day night cycle

---
+++ Assets/Scripts/Features/DayNightCycle/DayNightCycle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _lightSourceTransform;
+    private bool _rotating;
+
+    private void Start()
+    {
+        PlayTool.Instance.OnSecondPassedEvent.AddListener(RotateLightSource);
+       
+    }
+
+    private void RotateLightSource()
+    {
+        Debug.Log("rotating");
+        if (!_rotating)
+            StartCoroutine(Rotate(new Vector3(1, 0, 0), 1));
+    }
+
+    private IEnumerator Rotate(Vector3 angles, float duration)
+    {
+        _rotating = true;
+        Quaternion startRotation = _lightSourceTransform.rotation;
+        Quaternion endRotation = Quaternion.Euler(angles) * startRotation;
+        for (float t = 0; t < duration; t += Time.deltaTime)
+        {
+            _lightSourceTransform.rotation = Quaternion.Lerp(startRotation, endRotation, t / duration);
+            yield return null;
+        }
+        _lightSourceTransform.rotation = endRotation;
+        _rotating = false;
+    }
+}
