65964c5d47f8bf913c2cb8915e2ba3e3cb4335c5
Ben Rokah <benk0913@gmail.com>
2019-02-18 20:06:00 +0200

    rearrange and prop containers WIP

---
+++ Assets/Scripts/DataObject/Props/PropContainer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PropContainer", menuName = "DataObjects/Building/PropContainer", order = 2)]
+public class PropContainer : Prop
+{
+    [Tooltip("Size in weight")]
+    public int ContainerStorageSize;
+}
+
